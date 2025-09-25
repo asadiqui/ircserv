96fd089a511aa63ade52b04645a65e10637e76eb
Imad El Koubbi <iel-koub@e1r1p12.1337.ma>
2025-09-25 09:58:14 +0100

    .

--- main.cpp
+++ main.cpp
@@ -12,27 +12,18 @@ int main(int argc, char** argv)
         std::cout << "Usage: ./ircserv <port> <password>" << std::endl;
         return 1;
     }
-    int port;
-    try
+    for (int i = 0; argv[1][i]; i++)
     {
-        port = std::atoi(argv[1]);
-        if (port < 0 || port > 65535)
+        if (argv[1][i] < '0' || argv[1][i] > '9')
         {
-            std::cout << "Port must be between 0 and 65535!" << std::endl;
+            std::cout << "Port must be a number!" << std::endl;
             return 1;
         }
-        for (int i = 0; argv[1][i]; i++)
-        {
-            if (argv[1][i] < '0' || argv[1][i] > '9')
-            {
-                std::cout << "Port must be a number!" << std::endl;
-                return 1;
-            }
-        }
     }
-    catch (...)
+    int port = std::atoi(argv[1]);
+    if (port < 0 || port > 65535)
     {
-        std::cout << "Invalid port!" << std::endl;
+        std::cout << "Port must be between 0 and 65535!" << std::endl;
         return 1;
     }
     std::string password = argv[2];
